a1b53e96cebb95c8002b764aadbdfb69e5ee0c78
Muhammad Faizan Shabbir <Faizan.Shabbir.cs@uajk.edu.pk>
2026-05-23 15:08:01 +0500

    Add files via upload

---
+++ 04-var/var.js
@@ -0,0 +1,26 @@
+let rollNo = 20;
+
+let name = "Faizan";
+
+const regNo = 2704;
+
+city = "mzd";
+
+let postalAddress = 13001;
+
+var Street; //never use var because of block scope
+
+rollNo = 18;
+
+console.log(rollNo, name, regNo, city, Street);
+
+console.table([rollNo, name, regNo, city, Street]);
+
+
+{
+    let teaPrice = 60;
+    console.log(postalAddress);
+
+}
+
+console.log(teaPrice);
