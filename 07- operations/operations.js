d84c3f2f91562c65c7fc2945294133a8fba67760
Muhammad Faizan Shabbir <Faizan.Shabbir.cs@uajk.edu.pk>
2026-05-29 19:39:13 +0500

    Add files via upload

---
+++ 07- operations/operations.js
@@ -0,0 +1,27 @@
+let value = 50;
+let negValue = -value;
+
+// console.log(`Value: ${negValue}`);
+
+let firstName = "faizan";
+let secondName = "chauhan";
+
+let fullName = firstName + " shabbir " + secondName;
+console.log(`fullName: ${fullName}`);
+
+
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+
+// console.log(1 + 4 + "4")
+
+let teleCounter = 1000;
+++teleCounter;
+
+console.log(`teleCounter: ${teleCounter}`);
+
+console.log(+true);
+
+let num1 = 10;
+let num2 = 20;
+console.log(num1 + num2 + "6");
