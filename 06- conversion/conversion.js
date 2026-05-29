1a256e0a3708939651c8ec9a3a6d375f2a7f301d
Muhammad Faizan Shabbir <Faizan.Shabbir.cs@uajk.edu.pk>
2026-05-29 19:37:20 +0500

    Add files via upload

---
+++ 06- conversion/conversion.js
@@ -0,0 +1,47 @@
+let marks = "30";
+
+// console.log(marks);
+// console.log(typeof marks);
+
+let valueInNumber = Number(marks);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+let name = "faizan30";
+
+let nameInNumber = Number(name);
+// console.log(typeof nameInNumber);
+// console.log(nameInNumber);
+
+let scoredFifty = 1;
+let valueInBool = Boolean(scoredFifty);
+
+// console.log(scoredFifty);
+// console.log(typeof(valueInBool));
+// console.log(valueInBool);
+
+
+let scoredZero = null;
+
+let valueInNum = String(scoredZero);
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+
+let toBeAssigned = undefined;
+
+let conversion = Boolean(toBeAssigned);
+console.log(conversion);
+console.log(typeof(conversion));
+
+
+let sentence = "faizan is a good boy";
+let position = sentence.indexOf("good");
+
+console.log(position);
+console.log(sentence[10]);
+
+console.log(sentence.slice(0, 7));
+
+let trimmedSentence = sentence.trim();
+console.log(sentence.trim());
