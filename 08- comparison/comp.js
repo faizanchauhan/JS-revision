dd2b59095d39610d43883fe4a9a0d21da4761585
Muhammad Faizan Shabbir <Faizan.Shabbir.cs@uajk.edu.pk>
2026-05-29 19:39:47 +0500

    Add files via upload

---
+++ 08- comparison/comp.js
@@ -0,0 +1,21 @@
+// console.log(5 > 4);
+// console.log(5 < 4);
+// console.log(5 == 4);
+// console.log(5 >= 4);
+// console.log(5 <= 4);
+// console.log(5 != 4);
+
+console.log(null >= 0);
+console.log(null > 0);
+console.log(null == 0);
+
+
+console.log(undefined == 0);
+console.log(undefined <= 0);
+console.log(undefined >= 0);
+
+// Avoid these type of practices/conversions
+
+
+console.log("2" > 1);
+console.log("2" <= 1);
