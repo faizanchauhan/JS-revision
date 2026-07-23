335284007f51198c968daf0ebdaec6d8809e4df2
Muhammad Faizan Shabbir <Faizan.Shabbir.cs@uajk.edu.pk>
2026-07-24 00:47:15 +0500

    Add files via upload

---
+++ 26- switch case/switch.js
@@ -0,0 +1,22 @@
+//switch case
+
+let score = "two";
+
+switch (score) {
+    case "zero":
+        console.log(`one`);
+        break;
+    case "one":
+        console.log(`two`);
+        break;
+    case "two":
+        console.log(`three`);
+        break;
+    case "three":
+        console.log("four");
+        break;
+
+    default:
+        console.log(`default case activated`);
+        break;
+}
